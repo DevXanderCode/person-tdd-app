17019b43a686e03e0f14c70fc2d3b21e5642b1c3
Alex Nweke <nwekealexchinedu@gmail.com>
2021-02-11 10:05:15 +0100

    wrote my first test and made it pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// it('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const appWrapper = shallow(<App />);
+  });
 });
